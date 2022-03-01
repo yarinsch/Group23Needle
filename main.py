6cca4e8bab9ed341edffdafa6a8ba6a87a8004a6
Yarin Schiller <51716144+yarinsch@users.noreply.github.com>
2022-03-01 22:16:14 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from getData import *
-from DataTypes import *
-
-if __name__ == "__main__":
-    data = load_data_from_pkl("timelines_bronze.pkl")
-    all_types = set()
-    for game in data:
-        team = User(game)
-        print("num of types: ", len(all_types))
